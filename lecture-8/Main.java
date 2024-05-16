e23898209af1448ebe5276737fd7b4b650fb2983
krishna9809 <multiatoms980996@gmail.com>
2024-05-16 08:37:36 +0530

    lecture-8

---
+++ lecture-8/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[])
+    {
+        double variable1= Math.sqrt(6.0);
+        double variable2= Math.tan(20);
+        System.out.println("Square of 6 is:"+ variable1);
+        System.out.println("Tan of 20 is:"+ variable2);
+    }
+}
---
+++ lecture-8/importStatic.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class Stat {
+    public static void main(String args[])
+    {
+        //instead of Math.sqrt need to use only sqrt
+        double variable1= sqrt(6.0);
+        //instead of Math.tan need to use only tan
+        double variable2= tan(20);
+        //need not to use System in both the below statements
+        out.println("Square of 5 is:"+variable1);
+        out.println("Tan of 30 is:"+variable2);
+    }
+}
+
