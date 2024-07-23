c12d0cf8422f689c08474010ccbbd572daf62bbb
krishna9809 <multiatoms980996@gmail.com>
2024-07-24 03:36:07 +0530

    lecture-14

---
+++ lecture-014/Switch.java
@@ -0,0 +1,75 @@
+public class Switch {
+
+    public static void main(String[] args) {
+
+
+        // int day = 2;
+        
+        // String dayName = switch (day) {
+        //     case 1 -> "Sunday";
+        //     case 2 -> "Monday";
+        //     case 3 -> "Tuesday";
+        //     case 4 -> "Wednesday";
+        //     case 5 -> "Thursday";
+        //     case 6 -> "Friday";
+        //     case 7 -> "Saturday";
+        //     default -> "Invalid day";
+        // };
+
+        // System.out.println(dayName);
+
+    
+
+
+
+
+    // int day = 2;
+    // String result = switch (day) {
+    //     case 1 -> "Sunday";
+    //     case 2 -> {
+    //         System.out.println("Second day of the week");
+    //         yield "Monday";
+    //     }
+    //     case 3 -> {
+    //         int length = "Tuesday".length();
+    //         yield "Tuesday (" + length + " letters)";
+    //     }
+
+    //     default -> "noDay";
+    // };
+
+    // System.out.println(result);
+
+
+    
+
+// String textBlock = """
+//     This is a text block.
+//     It spans multiple lines.
+//     It is easy to read and maintain.
+//     """;
+
+//     System.out.println(textBlock);
+
+//     }
+
+
+    
+var name = "joker";
+var num = 3;
+
+// System.out.println(krishna);
+
+// var name = "krishna";
+System.out.println(name);
+System.out.println(num);
+
+
+}}
+
+
+
+
+
+
+
